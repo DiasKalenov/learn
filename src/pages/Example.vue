0477a8f1d9efd54bb2ec49db514369db665591dc
disa.kalenov2020@gmail.com <73879952+DiasKalenov@users.noreply.github.com>
2020-11-08 20:23:07 +0600

    v1

---
+++ src/pages/Example.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="wrapper-content wrapper-content--fixed">
+<section>
+    <div class="container">
+        <h1>Example Page</h1>
+        <p> {{ message }} </p>
+    </div>
+</section>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            message: null, 
+        }
+    },
+    created: () {
+      this.message = this.$store.getters.getMessage
+    },
+   
+}
+</script>
